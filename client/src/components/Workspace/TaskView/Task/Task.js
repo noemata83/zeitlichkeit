703b372759b0ca5458c7e6ba382ecbe5c1482c9d
Tucker McKinney <noemata83@gmail.com>
2018-05-08 08:16:16 -0400

    Sprints are sorted properly in the task view.

--- client/src/components/Workspace/TaskView/Task/Task.js
+++ client/src/components/Workspace/TaskView/Task/Task.js
@@ -5,7 +5,7 @@ import classes from './Task.css';
 import Sprint from '../../Sprint/Sprint';
 
 export default props => {
-    const sprintList = props.sprints.map((sprint, index) => (
+    const sprintList = [...props.sprints].sort((a, b) => b.start_time - a.start_time).map((sprint, index) => (
         <Sprint key={index} sprint={sprint} label={sprint.start_time.toDateString()} />
     ));
     const duration = props.sprints.reduce((total, sprint) => moment.duration(total).add(sprint.duration), moment.duration(0));
