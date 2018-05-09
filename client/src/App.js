59d072b477c303cd387c65177f9ce5602ca62249
Tucker McKinney <noemata83@gmail.com>
2018-05-09 11:15:28 -0400

    Add Material UI theme and convert navbar to AppBar Material-UI component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import Landing from './components/Landing';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#708690',
+      main: '#445963',
+      dark: '#1b3039',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      light: '#ffd95b',
+      main: '#ffa726',
+      dark: '#c77800',
+      contrastText: '#000000',
+    }
+  },
+  typography: {
+    fontFamily: 'Lato',
+    htmlFontSize: 10,
+  },
+  mixins: {
+    toolbar: {
+      minHeight: '4.5rem',
+      '@media (min-width:600px)': {
+        minHeight: '45px'
+      }
+
+    }
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={Landing} />
-          <Route path="/dashboard" component={Dashboard} />
-        </div>
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Landing} />
+            <Route path="/dashboard" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
--- client/src/components/UI/Header/Header.js
+++ client/src/components/UI/Header/Header.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import classes from './Header.css';
 import clockIcon from '../../../assets/img/clock-icon.svg';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import { withStyles, withTheme } from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
 
-const header = props => (
-    <nav className={classes.Header}>
-        <img src={clockIcon} className={classes.Logo} alt="TemporaLite" />
-        <div className={classes.Brand}>
-            Temporalite
-        </div>
+const header = props => {
+    const styles = {
+        root: {
+            flexGrow: 1,
+            height: '4.5rem',
+        },
+        title: {
+            fontSize: '2.5rem',
+            color: props.theme.palette.primary.contrastText,
+        padding: '1rem 2rem 1rem .5rem',
+        display: 'inline-block',
+        margin: '0 auto 0 0',
+        }
+    }
+    return (
+    <AppBar color="primary" style={styles.root} position='static'>
+        <Toolbar disableGutters>
+            <IconButton>
+                <img src={clockIcon} className={classes.Logo} alt="TemporaLite" />
+            </IconButton>
+            <Typography variant="title" style={styles.title}>    
+                Temporalite
+            </Typography>
         <div className={classes.NavMenuContainer}>
             <div className={classes.NavMenu}>
                 <div className={classes.NavMenuItem}>
@@ -21,7 +43,8 @@ const header = props => (
                 </div>
             </div>
         </div>
-    </nav>
-)
-
-export default header;
+        </Toolbar>
+    </AppBar>
+    );
+}
+export default withTheme()(header);
