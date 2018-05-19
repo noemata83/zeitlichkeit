e0962d9dfb618ec54d58b8890e1314bb948371b2
Tucker McKinney <noemata83@gmail.com>
2018-05-19 12:33:38 -0400

    Delete icon only visible when hovering over tasks in the project manager; Sweet!

--- client/src/components/ProjectManager/Project/Project.js
+++ client/src/components/ProjectManager/Project/Project.js
@@ -12,12 +12,20 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import ProjectMenu from "./ProjectMenu";
 import { Toolbar } from "@material-ui/core";
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = theme => ({
   title: {
-    display: 'inline-block',
-    borderBottom: `1px solid gray`,
+    display: "inline-block",
+    borderBottom: `1px solid gray`
+  },
+  listItemSecondaryAction: {
+    visibility: "hidden"
+  },
+  listItem: {
+    "&:hover $listItemSecondaryAction": {
+      visibility: "inherit"
+    }
   }
 });
 
@@ -25,19 +33,33 @@ const project = props => {
   const { project, tasks, classes, deleteTask } = props;
   return (
     <Grid item xs={12} md={6}>
-        <Toolbar>
-            <Typography variant="title" className={classes.title}>
-                {project.name}
-            </Typography>
-            <ProjectMenu id={project.id} name={project.name}/>
-        </Toolbar>
+      <Toolbar>
+        <Typography variant="title" className={classes.title}>
+          {project.name}
+        </Typography>
+        <ProjectMenu id={project.id} name={project.name} />
+      </Toolbar>
       <List>
         {tasks.map(task => (
-          <ListItem key={task.name} divider>
-            <Checkbox checked={task.completed} tabIndex={-1} />
+          <ListItem
+            key={task.name}
+            divider
+            classes={{
+              container: classes.listItem
+            }}
+          >
+            <Checkbox 
+              onChange={(event) => console.log(event.target.checked)}
+              checked={task.completed} 
+              tabIndex={-1} />
             <ListItemText primary={task.name} />
-            <ListItemSecondaryAction>
-              <IconButton aria-label="Delete Task" onClick={() => deleteTask(task.id)}>
+            <ListItemSecondaryAction
+              className={classes.listItemSecondaryAction}
+            >
+              <IconButton
+                aria-label="Delete Task"
+                onClick={() => deleteTask(task.id)}
+              >
                 <DeleteIcon />
               </IconButton>
             </ListItemSecondaryAction>
