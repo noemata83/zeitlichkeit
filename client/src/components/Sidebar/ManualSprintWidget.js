8325c73af4d337edf5af876ca4603c6c982da225
Tucker McKinney <noemata83@gmail.com>
2018-05-20 12:59:56 -0400

    Handling late night (PM->AM) sprints appropriately. Marked some duplicated code in widgets for refactoring.

--- client/src/components/Sidebar/ManualSprintWidget.js
+++ client/src/components/Sidebar/ManualSprintWidget.js
@@ -33,33 +33,20 @@ const styles = theme => ({
 
 const validate = values => {
     const errors = {};
-    if (values.start_time > values.end_time) {
-        errors.end_time = "End time must be after start time";
+    if (!values.project) {
+      errors.project = "Value is required.";
     }
     return errors;
 }
 
 class ManualSprintWidget extends Component {
   state = {
-    start_time: new Date(),
-    end_time: new Date(),
     task: "",
-    project: ""
   };
 
   updateTaskInput = (event, { newValue, method }) => {
     this.setState({
       task: newValue
-    });
-  };
-  changeStartHandler = event => {
-    this.setState({
-      start_time: new Date(event.target.value)
-    });
-  };
-  changeEndHandler = event => {
-    this.setState({
-      end_time: new Date(event.target.value)
     });
   };
 
@@ -70,6 +57,7 @@ class ManualSprintWidget extends Component {
   };
 
   checkIfTaskExists = (task, project) => {
+    // Duplicate
     if (
       !this.props.tasks
         .filter(existingtask => existingtask.project === project)
@@ -81,14 +69,24 @@ class ManualSprintWidget extends Component {
     return false;
   };
 
+  getEndDate = (startTime, endTime, date) => {
+    if (startTime < endTime) {
+      return date;
+    } else {
+      return new Date(new Date(date).getTime() + 86400000).toISOString().slice(0,10);
+    }
+  }
+
   onSubmit = values => {
     const { task } = this.state;
     const { project, date, start_time, end_time } = values;
+    // code below is duplicated, *up to resetForm()*
+    const end_date = this.getEndDate(start_time, end_time, date);
     const isNew = this.checkIfTaskExists(task, project);
     const sprint_data = {
       task: task,
       start_time: new Date(`${date}T${start_time}:00`).toISOString(),
-      end_time: new Date(`${date}T${end_time}:00`).toISOString()
+      end_time: new Date(`${end_date}T${end_time}:00`).toISOString()
     };
     if (isNew) {
       const task_data = {
@@ -120,7 +118,6 @@ class ManualSprintWidget extends Component {
         {project.name}
       </MenuItem>
     ));
-    console.log(this.state.task);
     return (
       <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <FormControl className={classes.formControl}>
@@ -135,11 +132,12 @@ class ManualSprintWidget extends Component {
             children={projectlist}
           />
           {/* For the time being, I am simply going to omit the Autosuggest field from 
-                        my redux-form. I don't need to do any validation, and the state-based logic
-                        I have already set up seems perfectly fine. It will make for a slightly
-                        disjointed submit process (pulling both from props and state, rather
-                        than treating all values in a unified way), but the setup cost just seems too
-                        high to justify at the moment. */}
+              my redux-form. I don't need to do any validation, and the state-based logic
+              I have already set up seems perfectly fine. It will make for a slightly
+              disjointed submit process (pulling both from props and state, rather
+              than treating all values in a unified way), but the setup cost just seems too
+              high to justify at the moment.
+              The problem for this is validation. */}
           <TaskAutosuggest
             updateTaskInput={this.updateTaskInput}
             task={this.state.task}
--- client/src/components/Sidebar/TimerWidget.js
+++ client/src/components/Sidebar/TimerWidget.js
@@ -1,174 +1,204 @@
-import React, { Component }  from 'react';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-import PlayArrow from '@material-ui/icons/PlayArrow';
-import Stop from '@material-ui/icons/Stop';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import TaskAutosuggest from '../UI/Forms/Autosuggest/TaskAutosuggest';
-import { addSprint, addTaskandSprint } from '../../store/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+import PlayArrow from "@material-ui/icons/PlayArrow";
+import Stop from "@material-ui/icons/Stop";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import TaskAutosuggest from "../UI/Forms/Autosuggest/TaskAutosuggest";
+import { addSprint, addTaskandSprint } from "../../store/actions";
 
 const styles = theme => ({
-    container: {
-        position: 'relative',
-      },
-      formControl: {
-        margin: theme.spacing.unit,
-        marginTop: 0,
-        minWidth: 120,
-        display: 'flex',
-        flexDirection: 'column',
-        height: '300px'
-      },
-      button: {
-          margin: theme.spacing.unit,
-      },
-      Timer: {
-          margin: theme.spacing.unit * 2,
-          display: 'block',
-          textAlign: 'center',
-          fontSize: '3rem'
-      }
+  container: {
+    position: "relative"
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    marginTop: 0,
+    minWidth: 120,
+    display: "flex",
+    flexDirection: "column",
+    height: "300px"
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  Timer: {
+    margin: theme.spacing.unit * 2,
+    display: "block",
+    textAlign: "center",
+    fontSize: "3rem"
+  }
 });
- 
+
 class TimerWidget extends Component {
-    state = {
+  state = {
+    timing: false,
+    counter: 0,
+    timer: null,
+    project: "",
+    task: "",
+    start_time: null
+  };
+
+  updateTaskInput = (event, { newValue, method }) => {
+    this.setState({
+      task: newValue
+    });
+  };
+
+  handleStart = () => {
+    let timer = setInterval(this.tick, 1000);
+    this.setState({
+      timing: true,
+      timer,
+      start_time: new Date().toISOString()
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  tick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  handleStop = () => {
+    clearInterval(this.state.timer);
+    const { task, project, start_time } = this.state;
+    if (task === "" || project === "") {
+      this.setState({
         timing: false,
-        counter: 0,
         timer: null,
-        project: '',
-        task: '',
+        counter: 0,
         start_time: null,
+        project: "",
+        task: ""
+      });
+      return;
     }
-
-    updateTaskInput = (event, { newValue, method }) => {
-        this.setState({
-            task: newValue
-        })
+    const end_time = new Date().toISOString();
+    // Code below is duplicated
+    const isNew = this.checkIfTaskExists(this.state.task, this.state.project);
+    const sprint_data = {
+      task,
+      start_time,
+      end_time
+    };
+    if (isNew) {
+      const task_data = {
+        name: task,
+        project: {
+          name: project
+        },
+        categories: [],
+        completed: false,
+        sprint_set: []
+      };
+      this.props.addTaskAndSprint(task_data, sprint_data);
+    } else {
+      this.props.addSprint(sprint_data);
     }
+    this.setState({
+      timing: false,
+      timer: null,
+      counter: 0,
+      start_time: null,
+      project: "",
+      task: ""
+    });
+  };
 
-    handleStart = () => {
-        let timer = setInterval(this.tick, 1000);
-        this.setState({
-            timing: true,
-            timer,
-            start_time: new Date().toISOString(),
-        });
+  checkIfTaskExists = (task, project) => {
+    // Duplicated
+    if (
+      !this.props.tasks
+        .filter(existingtask => existingtask.project === project)
+        .map(existingtask => existingtask.name)
+        .includes(task)
+    ) {
+      return true;
     }
+    return false;
+  };
 
-    handleChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    tick = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-
-    handleStop = () => {
-        clearInterval(this.state.timer);
-        const {task, project, start_time} = this.state;
-        if (task === '' || project === '' ) {
-            this.setState({
-                timing: false,
-                timer: null,
-                counter: 0,
-                start_time: null,
-                project: '',
-                task: ''
-            });
-            return;
-        }
-        const end_time = new Date().toISOString();
-        const isNew = this.checkIfTaskExists(this.state.task, this.state.project);
-        const sprint_data = {
-            task,
-            start_time,
-            end_time
-        };
-        if (isNew) {
-            const task_data = {
-                "name": task,
-                "project": {
-                    "name": project
-                },
-                "categories": [],
-                "completed": false,
-                "sprint_set": []
+  displayTimer = counter => {
+    const hh = Math.floor(counter / 3600);
+    const mm = Math.floor((counter % 3600) / 60);
+    const ss = counter - hh * 3600 - mm * 60;
+    return (
+      `${hh}`.padStart(2, "0") +
+      ":" +
+      `${mm}`.padStart(2, "0") +
+      ":" +
+      `${ss}`.padStart(2, "0")
+    );
+  };
+  render() {
+    const { projects, classes } = this.props;
+    const projectlist = ["None", ...projects].map((project, index) => (
+      <MenuItem key={index} value={project.name}>
+        {project.name}
+      </MenuItem>
+    ));
+    return (
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="project_select">Project</InputLabel>
+          <Select
+            name="project"
+            value={this.state.project}
+            onChange={this.handleChange}
+          >
+            {projectlist}
+          </Select>
+          <TaskAutosuggest
+            updateTaskInput={this.updateTaskInput}
+            task={this.state.task}
+            project={this.state.project || ""}
+          />
+          <div className={classes.Timer}>
+            {this.state.timing
+              ? this.displayTimer(this.state.counter)
+              : "00:00:00"}
+          </div>
+          <Button
+            variant="raised"
+            color="secondary"
+            onClick={() =>
+              this.state.timing ? this.handleStop() : this.handleStart()
             }
-            this.props.addTaskAndSprint(task_data, sprint_data);
-        } else {
-            this.props.addSprint(sprint_data);
-        }
-        this.setState({
-            timing: false,
-            timer: null,
-            counter: 0,
-            start_time: null,
-            project: '',
-            task: ''
-        });
-    }
-
-    handleRecordSprint = (counter) => {
-
-    }
-
-    checkIfTaskExists = (task, project) => {
-        if (!this.props.tasks.filter(existingtask => existingtask.project === project).map(existingtask => existingtask.name).includes(task)) {
-            return true;
-        }
-        return false;
-    }
-
-    displayTimer = (counter) => {
-        const hh = Math.floor(counter / 3600);
-        const mm = Math.floor((counter % 3600) / 60);
-        const ss = counter - (hh * 3600) - (mm * 60);
-        return `${hh}`.padStart(2, '0') + ":" + `${mm}`.padStart(2, '0') + ":" + `${ss}`.padStart(2, '0');
-    }
-    render() {
-        const { projects, classes } = this.props;
-        const projectlist = ['None', ...projects].map((project, index) => <MenuItem key={index} value={project.name}>{project.name}</MenuItem>);   
-        return (            <div>
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor='project_select'>Project</InputLabel>
-                        <Select
-                            name="project"
-                            value={this.state.project}
-                            onChange={this.handleChange}
-                        >{projectlist}</Select>
-                    <TaskAutosuggest updateTaskInput={this.updateTaskInput} task={this.state.task} project={this.state.project || ''} />
-                    <div className={classes.Timer}>
-                        {this.state.timing ? this.displayTimer(this.state.counter) : "00:00:00" }
-                    </div>
-                    <Button variant="raised" color="secondary" onClick={() => this.state.timing ? this.handleStop() : this.handleStart()}>{this.state.timing ? <Stop /> : <PlayArrow />}</Button>
-                </FormControl>
-            </div>
-        );
-    }
+          >
+            {this.state.timing ? <Stop /> : <PlayArrow />}
+          </Button>
+        </FormControl>
+      </div>
+    );
+  }
 }
-
 
 const mapStateToProps = state => {
-    return {
-        projects: state.workspace.project_set,
-        tasks: state.workspace.task_set,
-    }
-}
+  return {
+    projects: state.workspace.project_set,
+    tasks: state.workspace.task_set
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addTaskAndSprint: (task_data, sprint_data) => dispatch(addTaskandSprint(task_data, sprint_data)),
-        addSprint: (sprint_data) => dispatch(addSprint(sprint_data)),
-    }
-}
+  return {
+    addTaskAndSprint: (task_data, sprint_data) =>
+      dispatch(addTaskandSprint(task_data, sprint_data)),
+    addSprint: sprint_data => dispatch(addSprint(sprint_data))
+  };
+};
 
 TimerWidget = withStyles(styles)(TimerWidget);
 
