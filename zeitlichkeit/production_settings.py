477c1955fa6218bf8c1bf96eb3b59f4caef5776e
Tucker McKinney <noemata83@gmail.com>
2018-05-25 16:22:07 +0000

    Modified production settings and gitignore

--- zeitlichkeit/production_settings.py
+++ zeitlichkeit/production_settings.py
@@ -1,5 +1,12 @@
 from .settings import *
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['206.81.4.24',]
+SECRET_KEY=os.environ['SECRET']
+
+STATIC_ROOT=os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
@@ -9,4 +16,15 @@ WEBPACK_LOADER = {
             'BUNDLE_DIR_NAME': 'bundles/',
             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
         }
+}
+
+DATABASES = {
+    'default': {
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'temporaldb',
+	'USER': 'tempus',
+	'PASSWORD': 'time is the horizon of every understanding',
+	'HOST': 'localhost',
+	'PORT': '',
+    }
 }
