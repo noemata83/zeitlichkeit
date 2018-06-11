174f8019dfd6490aa5ca6592f38b396e34d108f6
Tucker McKinney <noemata83@gmail.com>
2018-06-11 16:30:17 -0400

    Attempting a service worker problem fix.

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import reducers from './store/reducers';
 import './index.css';
@@ -16,4 +15,3 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 // eslint-disable-next-line
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
