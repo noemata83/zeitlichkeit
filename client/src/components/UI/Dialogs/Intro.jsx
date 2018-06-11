1c7de2e0aec7cc33c8b55efc57de9f571372397b
Tucker McKinney <noemata83@gmail.com>
2018-06-11 17:29:04 -0400

    Updated the intro dialog to warn about weird production UI breakage

--- client/src/components/UI/Dialogs/Intro.jsx
+++ client/src/components/UI/Dialogs/Intro.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
 import { DialogTitle } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
 const IntroDialog = (props) => {
     const {
@@ -20,6 +21,13 @@ const IntroDialog = (props) => {
         <div style={{ padding: '2rem' }}>
             <DialogTitle>Welcome to Temporalite!</DialogTitle>
             <div style={{ fontSize: '1.8rem', padding: '0 3rem' }} >
+              <p>
+                <strong>June 11, 2018 Update</strong>: An issue in the Material UI library is
+                occasionally causing the styling of the user interface of the production build
+                to break. If, when you login, things look <em>weird</em>, simply reload the page.
+                We're working on it!
+              </p>
+                <Divider />
                 <p>
                   Temporalite aims to be a lightweight time-tracker and productivity management
                   tool. Temporalite allows you (and, ultimately, your team) to track, categorize,
@@ -33,7 +41,7 @@ const IntroDialog = (props) => {
                 <p>
                   To get started, create a user by clicking 'Register': enter any user name and password,
                   and you will be taken to the dashboard, where you can begin logging your time,
-                  building your todo list, and visualizing your produtivity. Enjoy.
+                  building your todo list, and visualizing your productivity. Enjoy.
                 </p>
             </div>
             <div style={{textAlign: 'center'}}>
